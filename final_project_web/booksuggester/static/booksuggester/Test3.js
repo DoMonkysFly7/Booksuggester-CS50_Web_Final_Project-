4fb2ec82bb07ed3dbb119a63ae3db582b727ff70
DoMonkysFly7 <opreastefan9926@gmail.com>
2023-01-04 22:22:53 +0200

    Third commit

--- final_project_web/booksuggester/static/booksuggester/Test3.js
+++ final_project_web/booksuggester/static/booksuggester/Test3.js
@@ -40,13 +40,19 @@ class App extends React.Component {
         const logo_container = document.querySelector('#logo_container');
         const logo = document.querySelector('#logo');
         const quotesContainer = document.querySelector('#quotes_container');
+        const answerContainer = document.querySelector('#answer_container');
         const question = document.querySelector('#question');
+        const answer = document.querySelectorAll('#answer');
 
         logo_container.className = 'colorTransition';
-        quotesContainer.className = 'colorTransition';
-        question.style.animation = 'runRight 1.1s ease-in-out, QuestionReappear 2.7s 1.1s ease-in-out, Question-colorize 3.8s reverse';
-        
         logo.style.animation = 'transitionIn 1.5s ease-in-out reverse forwards';
+        quotesContainer.className = 'colorTransition';
+
+        question.style.animation = 'runRight 1.1s ease-in-out, QuestionReappear 2.7s 1.1s ease-in-out, Question-colorize 3.9s reverse';
+        answerContainer.style.animation = 'runLeft 2.2s ease-in-out, AnswerReappear 2.7s 1.1s ease-in-out';
+        answer.forEach(a => {
+            a.style.animation = 'Answer-colorize 3.9s reverse';
+        })
 
         // Delay this until the above 'question.style.animation' is done
         window.setTimeout(this.qReset, 3750);
@@ -55,7 +61,11 @@ class App extends React.Component {
     qReset = async () => {
 
         // alert('success');
+        document.querySelectorAll('#answer').forEach(a => {
+            a.style.animation = 'none';
+        });
         document.querySelector('#question').style.animation = 'none';
+        document.querySelector('#answer_container').style.animation = 'none';
     }
 
     render() {
@@ -65,7 +75,9 @@ class App extends React.Component {
                     <div id="question"> {this.state.questions[this.state.question_number]} </div>
                 </div>
                 <div id="answer_container">
-                    <div id="answer" onClick={this.changeQuestion}> Possible answer </div>
+                    <div id="answer" onClick={this.changeQuestion}> Possible answer 1 </div>
+                    <div id="answer" onClick={this.changeQuestion}> Possible answer 2 </div>
+                    <div id="answer" onClick={this.changeQuestion}> Possible answer 3 </div>
                 </div>
             </div>
         )};
