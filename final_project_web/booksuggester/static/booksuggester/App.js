fc58fe5878ea08f3d95204ecbf50734e8b682988
DoMonkysFly7 <opreastefan9926@gmail.com>
2023-01-09 06:07:56 +0200

    Commit Eight - Got how to do the API book collection

--- final_project_web/booksuggester/static/booksuggester/App.js
+++ final_project_web/booksuggester/static/booksuggester/App.js
@@ -46,7 +46,8 @@ class App extends React.Component {
                 start_question: "Emotional or thinking type?",
                 question_number: 0,
                 q1_response: "", 
-                books: [],
+                recommended_books: [],
+                track_answers: [],
                 emotional_q_set: ["","You're kinda romantic, huh?", "Are you attentive?", "Reading or watching?", "Past or future?", "This world sucks?", "Are you artsy?", "Really young ppl write well?",
                                 "Wanna get spooked baby?", "You like Berserk (manga)?", "Historical human suffering?", "Ur God's cool?", "U listen to important ppl?", "You trust me?"],
                 emotional_r_set: ["Yes", "No", "Kinda", "Nope", "Watching", "Reading. Duh.", "I prefer the past", "I prefer the future", "Another one's cool", "I guess 'tis fine","Yeah!", "Nein",
@@ -56,6 +57,15 @@ class App extends React.Component {
                                 "U go visiting castles bro?", "U listen to celebrities?", "To be a spooked fruit?", "U love me? <3"],
                 thinking_r_set: ["Dosto!", "My Harriet babe!", "Obviously.", "Who?", "Always", "Yeahhh right", "F her/him", "Idk but F her/him", "All the time.", "Huh?", "Y", "N", "Indubitably", 
                                 "Who cares?", "Banana!", "NO!", "SPELLBLINDINGLY BEAUTIFUL!", "No xoxo"],
+              
+                // Trebuie sa fie un numar par de raspunsuri, astfel react nu poate sa rendeze 'written' deoarece unul din cele doua nu ar exista
+                thinkging_r_mock_set: [ {written:"Dosto!", hidden:"literature"}, 
+                                        {written:"My Harriet babe!", hidden:"-"}, 
+                                        {written:"Obviously", hidden:"-"},
+                                        {written:"Who?", hidden:"-"},
+                                        {written:"Always", hidden:"-"},
+                                        {written:"Yeahhh right", hidden:"-"}],
+
                 iter_number1: -2,
                 iter_number2: -1,
             }
@@ -69,13 +79,13 @@ class App extends React.Component {
         
         let answer = event.target.innerHTML;
 
-        if (answer == "Emotional") {
+        if (answer === "Emotional") {
             console.log('You are emotional!');
             this.setState({
                 q1_response: "Emotional"
             });
 
-        } else if (answer == "Thinking") {
+        } else if (answer !== "Emotional") {
             console.log('You like to think!');
             this.setState({
                 q1_response: "Thinking"
@@ -83,10 +93,23 @@ class App extends React.Component {
         }
 
         // console.log(`${answer} : ${question_number}`);
-        this.ApiOutput_Emotional();
+        this.ApiOutput_Emotional(answer);
     }
 
-    ApiOutput_Emotional = () => {
+    ApiOutput_Emotional = (written_answer) => {
+        // Based on the answer first answer, we can know in which answers list are we: thinking or emotional
+        // Based on each 'written' answer, we can iterate trhough the list of the answers and find its pair value
+        // The pair value is sent to OpenLibrary API for subjects to add books to the user's list.
+        
+        if (this.state.q1_response === "Thinking") {
+            this.state.thinkging_r_mock_set.forEach(item => {
+                if(written_answer === item.written) {
+                    // The argument for the API fetch
+                    console.log(item.hidden);
+                };
+            }) 
+        }
+
         // For subjects that have multiple subcategories (history and biography, for example). API searches through the /search 
         // method may be required, instead of /subjects method
         console.log('Response registered');
@@ -148,8 +171,8 @@ class App extends React.Component {
                         <div id="question"> {this.state.thinking_q_set[this.state.question_number]} </div>
                     </div>
                     <div id="answer_container">
-                        <div id="answer" onClick={this.changeQuestion}> {this.state.thinking_r_set[this.state.iter_number1]} </div>
-                        <div id="answer" onClick={this.changeQuestion}> {this.state.thinking_r_set[this.state.iter_number2]} </div>
+                        <div id="answer" onClick={this.changeQuestion}>{this.state.thinkging_r_mock_set[this.state.iter_number1].written}</div>
+                        <div id="answer" onClick={this.changeQuestion}>{this.state.thinkging_r_mock_set[this.state.iter_number2].written}</div>
                     </div>
                 </div>
         )} else {
@@ -160,8 +183,8 @@ class App extends React.Component {
                         <div id="question"> {this.state.start_question} </div>
                     </div>
                     <div id="answer_container">
-                        <div id="answer"  onClick={this.changeQuestion}>Emotional</div>
-                        <div id="answer" onClick={this.changeQuestion}>Thinking</div>
+                        <div id="answer" onClick={this.changeQuestion}>Emotional</div>
+                        <div id="answer"onClick={this.changeQuestion}>Thinking</div>
                     </div>
                 </div>
             )};
