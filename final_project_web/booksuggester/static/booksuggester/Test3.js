aaac7ba683ea338b4b4301aa0846efc84d31de82
DoMonkysFly7 <opreastefan9926@gmail.com>
2023-01-19 08:20:19 +0200

    Fixing API errors

--- final_project_web/booksuggester/static/booksuggester/Test3.js
+++ final_project_web/booksuggester/static/booksuggester/Test3.js
@@ -1,8 +1,9 @@
 const author_choices = ['works/OL166894W','works/OL102749W','works/OL20600W','works/OL1168083W','works/OL21164750W', 'works/OL1268413W','works/OL498463W', 'works/OL5819456W', 'works/OL103123W',
-'books/OL7383896M'];
+'works/OL16457776W'];
 //Order:"Crime and punishment", "Moby Dick", "Gulliver's travels", "1984", "Amusing ourselves to death", "Man's search for meaning"// "The Trial", "The Book Thief", "Fahrenheit 451", "Petersburg Tales"
 
-const author_choice_temp = []
+const author_choice_temp = [];
+const limit = 20;
 
 class Quotes extends React.Component {
 
@@ -50,38 +51,45 @@ class App extends React.Component {
             this.state = {
                 question_number: 0,
                 
-                suggestions: [],
-                books: [],
+                suggestions: [{"":""}],
+                books: [{"":""}],
 
-                questions: ["This world sucks?","Be a spooky-fruity?","Who's your country's leader?","Teens write well?","U listen to 'important' ppl?","Love me?","Which u choose?",
-                "Historical human suffering?"],
 
-                // questions: ["This world sucks?","Be a spooky-fruity?","Who's your country's leader?","Teens write well?","U listen to 'important' ppl?","Love me?","Which u choose?",
+                // questions: ["This world sucks?","Be a spooky-fruity?","Who's your country's leader?","Ain't youth brains cool?","U listen to 'important' ppl?","Love me?","Where to travel?",
                 // "Historical human suffering?","You're kinda romantic, huh?","Like reading situations/ppl?","You like Berserk (manga)?","Ur God's cool?","Dostoevsky or Harry Styles?",
                 // "You know Carl Jung?","Go recite poetry nerd","U go theater?","U think bout society'n stuff?","Ur an art student?","Long books are too long?","I give u bloody stories!",
-                // "People talking about themselves?"],
+                // "Ppl tlking bout themselves?"],
 
-                answers: [{written:"Another one's cool", hidden:"fantasy"},{written:"I guess 'tis fine", hidden:"-", quote:"Really?"}, {written:"Banana!", hidden:"horror"},{written:"Neh", hidden:"-"},
-                {written:"I hate her/him", hidden:"political_science", quote:"Don't we all."},{written:"Idk", hidden:"-"},{written:"U insane?", hidden:"-"},{written:"I am insane.",hidden:"young_adult_fiction",
-                quote:"Figured as much"},{written:"Indubitably", hidden:"biography"},{written:"Like who? Elon? =))", hidden:"-",quote:"=))"},{written:"I want ur babies!", hidden:"-",
-                quote:"I am flattered."},{written:"MAY wnt ur babies",hidden:"-",quote:"What do you mean 'may'???"},
-                {written:"The past", hidden:"history"},{written:"The future", hidden:"science_fiction"},{written:"Y!Y!F U TOO!",hidden:"world_war_ii",quote:"<3"},{written:"Umm...no?",hidden:"-"}
+                questions: ["You're kinda romantic, huh?","Like reading situations/ppl?","You like Berserk (manga)?","Ur God's cool?","Dostoevsky or Harry Styles?",
+                "You know Carl Jung?","Go recite poetry nerd","U go theater?","U think bout society'n stuff?","Ur an art student?","Long books are too long?","I give u bloody stories!",
+                "Ppl tlking bout themselves?"],
+
+                answers: [{written:"Yes", hidden:"romance"},{written:"No", hidden:"thriller"},{written:"Elementary.", hidden:"mystery_and_detective_stories"},{written:"Nope", hidden:"-"},
+                {written:"Indeed, struggler.", hidden:"dark_fantasy", quote:"That's my boy!"},{written:"Was ist das?", hidden:"-",quote:"Why do u even exist"},
+                {written:"AMEN BRO!",hidden:"religion",quote:"Chilllll..."},{written:"I am my own God!", hidden:"-"},
+                {written:"Dosto!", hidden:"classics"},{written:"My Harriet babe!",hidden:"-",quote:"Pls never contact me"},{written:"Obviously", hidden:"psychoanalysis"},{written:"Who?", hidden:"-",
+                quote:"Bro..."},{written:"Mkay",hidden:"poetry",quote:"That's a good boy-girl!"},{written:"No ty", hidden:"-"},{written:"Yes!!!", hidden:"play"},{written:"Nein", hidden:"-",
+                quote:"Missing out really"},{written:"Always",hidden:"society",quote:"Check out the anime 'Psycho-Pass"},{written:"Yeahhh right",hidden:"-",quote:"Not like ur parte of it"},
+                {written:"That I am!",hidden:"art_history",quote:"Only an art student would care about art history really",},{written:"God no",hidden:"-",quote:"So you are normal then"},
+                {written:"Man yes...",hidden:"short_stories",quote:"You sure don't got time for that"},{written:"I like them",hidden:"-",quote:"U want a prize or smth?"},{written:"Please no!",
+                hidden:"historical_-_general",quote:"You so fragile!"},{written:"YES!",hidden:"violence",quote:"Get help."},{written:"...Yes?",hidden:"autobiography",quote:"???"},{written:"Who cares??",
+                hidden:"-",quote:"Indeed."}
                 ,{written: "", hidden:"-"},{written: "", hidden:"-"}],
                 
-                // answers: [{written:"Another one's cool", hidden:"fantasy"},{written:"I guess 'tis fine", hidden:"-", quote:"Really?"}, {written:"Banana!", hidden:"horror"},{written:"Neh", hidden:"-"},
-                // {written:"I hate her/him", hidden:"political_science", quote:"Don't we all."},{written:"Idk", hidden:"-"},{written:"U insane?", hidden:"-"},{written:"I am insane.",hidden:"young_adult_fiction",
+                // answers: [{written:"Another one's cool", hidden:"magic"},{written:"I guess 'tis fine", hidden:"-", quote:"Really?"}, {written:"Banana!", hidden:"horror"},{written:"Neh", hidden:"-"},
+                // {written:"I hate her/him", hidden:"political_science", quote:"Don't we all."},{written:"Idk", hidden:"-"},{written:"Don't think so", hidden:"-"},{written:"Of coursely.",hidden:"young_adult_fiction",
                 // quote:"Figured as much"},{written:"Indubitably", hidden:"biography"},{written:"Like who? Elon? =))", hidden:"-",quote:"=))"},{written:"I want ur babies!", hidden:"-",
                 // quote:"I am flattered."},{written:"MAY wnt ur babies",hidden:"-",quote:"What do you mean 'may'???"},
-                // {written:"The past", hidden:"history"},{written:"The future", hidden:"science_fiction"},{written:"Y!Y!F U TOO!",hidden:"world_war_ii",quote:"<3"},{written:"Umm...no?",hidden:"-",
+                // {written:"The past", hidden:"history"},{written:"The future", hidden:"science_fiction"},{written:"Y!Y!F U TOO!",hidden:"world_war",quote:"<3"},{written:"Umm...no?",hidden:"-",
                 // quote:":("},{written:"Yes", hidden:"romance"},{written:"No", hidden:"thriller"},{written:"Elementary.", hidden:"mystery_and_detective_stories"},{written:"Nope", hidden:"-"},
-                // {written:"Indeed, struggler.", hidden:"historical_fiction", quote:"That's my boy!"},{written:"Was ist das?", hidden:"-",quote:"Why do u even exist"},
+                // {written:"Indeed, struggler.", hidden:"dark_fantasy", quote:"That's my boy!"},{written:"Was ist das?", hidden:"-",quote:"Why do u even exist"},
                 // {written:"AMEN BRO!",hidden:"religion",quote:"Chilllll..."},{written:"I am my own God!", hidden:"-"},
-                // {written:"Dosto!", hidden:"literature"},{written:"My Harriet babe!",hidden:"-",quote:"Pls never contact me"},{written:"Obviously", hidden:"psychology"},{written:"Who?", hidden:"-",
-                // quote:"Bro..."},{written:"Mkay",hidden:"poetry",quote:"That's a good boy-girl!"},{written:"No ty", hidden:"-"},{written:"Yeah!!!", hidden:"plays"},{written:"Nein", hidden:"-",
-                // quote:"Missing out really"},{written:"Always",hidden:"anthropology",quote:"Check out the anime 'Psycho-Pass"},{written:"Yeahhh right",hidden:"-",quote:"Not like ur parte of it"},
+                // {written:"Dosto!", hidden:"classics"},{written:"My Harriet babe!",hidden:"-",quote:"Pls never contact me"},{written:"Obviously", hidden:"psychoanalysis"},{written:"Who?", hidden:"-",
+                // quote:"Bro..."},{written:"Mkay",hidden:"poetry",quote:"That's a good boy-girl!"},{written:"No ty", hidden:"-"},{written:"Yes!!!", hidden:"play"},{written:"Nein", hidden:"-",
+                // quote:"Missing out really"},{written:"Always",hidden:"society",quote:"Check out the anime 'Psycho-Pass"},{written:"Yeahhh right",hidden:"-",quote:"Not like ur parte of it"},
                 // {written:"That I am!",hidden:"art_history",quote:"Only an art student would care about art history really",},{written:"God no",hidden:"-",quote:"So you are normal then"},
                 // {written:"Man yes...",hidden:"short_stories",quote:"You sure don't got time for that"},{written:"I like them",hidden:"-",quote:"U want a prize or smth?"},{written:"Please no!",
-                // hidden:"social_life_and_customs",quote:"You so fragile!"},{written:"YES!",hidden:"-",quote:"Get help."},{written:"...Yes?",hidden:"autobiography",quote:"???"},{written:"Who cares??",
+                // hidden:"historical_-_general",quote:"You so fragile!"},{written:"YES!",hidden:"violence",quote:"Get help."},{written:"...Yes?",hidden:"autobiography",quote:"???"},{written:"Who cares??",
                 // hidden:"-",quote:"Indeed."}
                 // ,{written: "", hidden:"-"},{written: "", hidden:"-"}],
 
@@ -106,11 +114,16 @@ class App extends React.Component {
                 fetch(`https://openlibrary.org/${workitem}.json`)
                 .then(response => response.json())
                 .then(book => {
-                    // Print result
-                    const description = book['description'];
+                    let description = book['description'];
+                    if(book['description']['type']){
+                        description = book['description']['value'];
+                    } else {
+                        description = book['description']
+                    }
+                    console.log(description);
                     const author_array = book['authors'][0];
                     const author_key = author_array['author']['key'];
-                    const cover_id = book['covers'][16];
+                    const cover_id = book['covers'][0];
                     const title = book['title'];
                     const source_img = `https://covers.openlibrary.org/b/id/${cover_id}-M.jpg`    
                     
@@ -124,47 +137,62 @@ class App extends React.Component {
                         console.log(error);
                     })
                 })
+                .catch(error => {
+                    console.log(error);
+                })
             })
-        
-            this.setState(state => ({
-                suggestions: [...state.suggestions, author_choice_temp]
-            }));
+            this.state.suggestions.push(author_choice_temp);
         } else {
-            // Check if 'axios' can be used
-            // Continue here, what happens if we got a regular question -> get the hidden value, get first ten books, choose one randomly, get its info, add it to the list
             this.state.answers.forEach((answer) => {
                 if(answer.written === written_answer && answer.hidden !== '-'){
                     const subject = answer.hidden;
-                    let temp = [];
-                    fetch(`https://openlibrary.org/subjects/${subject}.json?limit=10`)
+                    console.log(subject);
+                    let temp = [{"":""}];
+                    fetch(`https://openlibrary.org/subjects/${subject}.json?limit=${limit}`)
                     .then(res => res.json())
                     .then(output => {
                         output['works'].forEach((book) => {
-                            console.log(book);
-                            const description = book['description'];
-                            const author_array = book['authors'][0];
-                            const author_key = author_array['key'];
-                            const cover_id = book['cover_id'];
-                            console.log(cover_id);
-                            const title = book['title'];
-                            const source_img = `https://covers.openlibrary.org/b/id/${cover_id}-M.jpg`
-                            
-                            fetch(`https://openlibrary.org${author_key}.json`)
+                            fetch(`https://openlibrary.org${book['key']}.json`)
                             .then(response => response.json())
-                            .then(result => {
-                                const author = result['personal_name'];
-                                temp.push({title,author,description,source_img});
-                                if(temp.length === 10){
-                                    // console.log(temp);
-                                    const get_random = Math.ceil(Math.random() * 10);
-                                    console.log(temp[get_random]);
-                                    this.setState(state => ({
-                                        books: [...state.books, temp[get_random]]
-                                    }))
-                                }
-                            })
-                            .catch(error => {
-                                console.log(error);
+                            .then(book => {
+                                // console.log(book['subject_times']);
+                                let description = book['description'];
+                                // if(book['description']['type']){
+                                //     description = book['description']['value'];
+                                // } else {
+                                //     description = book['description']
+                                // }
+                                const author_array = book['authors'][0];
+                                const author_key = author_array['author']['key'];
+                                const cover_id = book['covers'][0];
+                                const title = book['title'];
+                                const source_img = `https://covers.openlibrary.org/b/id/${cover_id}-M.jpg`    
+                                
+                                fetch(`https://openlibrary.org${author_key}.json`)
+                                .then(response => response.json())
+                                .then(result => {
+                                    const author = result['personal_name'];
+                                    temp.push({title,author,description,source_img});
+                                    if(temp.length === limit){
+                                        let get_random = Math.ceil(Math.random() * 20);;
+                                        if(temp[get_random] === undefined) {
+                                            this.state.books.push(temp[get_random - 1]);
+                                            if(temp[get_random - 1]['description']['value']) {
+                                                temp[get_random - 1]['description'] = temp[get_random - 1]['description']['value']
+                                            }
+                                            console.log(temp[get_random - 1]['description']);
+                                        } else {
+                                            this.state.books.push(temp[get_random]);
+                                            if(temp[get_random]['description']['value']) {
+                                                temp[get_random]['description'] = temp[get_random]['description']['value']
+                                            }
+                                            console.log(temp[get_random - 1]['description']);
+                                        }
+                                    }
+                                })
+                                .catch(error => {
+                                    console.log(error);
+                                })
                             })
                         })
                     })
@@ -206,11 +234,26 @@ class App extends React.Component {
     render()
     {   
         console.log(this.state.books);
-        console.log(this.state.suggestions);
+        // console.log(this.state.suggestions);
 
         this.state.iter_number1 += 2;
         this.state.iter_number2 += 2;
 
+        if(this.state.questions[this.state.question_number] === undefined){
+            const get_final_random = Math.ceil(Math.random() * this.state.books.length);
+            const rand_final_choice = this.state.books[get_final_random];
+            console.log(get_final_random);
+            console.log(rand_final_choice);
+            // Pare sa nu ia ultima carte in considerare - wait function with a loading screen?
+            return(
+                <div>
+                    <h6>{rand_final_choice['title']}</h6>
+                    <h6>{rand_final_choice['author']}</h6>
+                    <h6>{rand_final_choice['description']}</h6>
+                    <img src={rand_final_choice['source_img']}/>
+                </div>
+            )
+        }
         return(
             <div id="app_container">
                 <div id="question_container">
@@ -223,8 +266,6 @@ class App extends React.Component {
             </div>
         )}
 }
-
-
 ReactDOM.render(<App />, document.querySelector('#app'));
 
 
